173f6f2635f59f667035b32d01205f15a1554649
Eliot Cowley <elcowle@microsoft.com>
2018-10-31 16:00:59 -0700

    Added functionality to update dates in all files in specified folder

---
+++ UpdateDate/UpdateDateFromFolder/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace UpdateDateFromFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args[0];
+            var files = Directory.EnumerateFiles(path);
+            string _dateText = @"ms.date";
+
+            foreach (var file in files)
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+
+                using (StreamReader streamReader = new StreamReader(file))
+                {
+                    string currentLine;
+
+                    while (streamReader.Peek() >= 0)
+                    {
+                        currentLine = streamReader.ReadLine();
+
+                        if (currentLine.Contains(_dateText))
+                        {
+                            currentLine = _dateText + @": " + DateTime.Today.ToString("d");
+                        }
+
+                        stringBuilder.AppendLine(currentLine);
+                    }
+                }
+
+                using (StreamWriter streamWriter = new StreamWriter(file))
+                {
+                    streamWriter.Write(stringBuilder.ToString());
+                }
+            }
+        }
+    }
+}
