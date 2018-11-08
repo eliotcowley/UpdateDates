6e262afc95ac3022ea3cf7b1d37b938e4eddeab6
Eliot Cowley <elcowle@microsoft.com>
2018-11-07 16:04:08 -0800

    Added program to remove some metadata

---
+++ UpdateDate/RemoveMetadata/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoveMetadata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args[0];
+            var files = Directory.EnumerateFiles(path);
+            string prodString = @"ms.prod";
+            string techString = @"ms.technology";
+
+            foreach (var file in files)
+            {
+                StringBuilder stringBuilder = new StringBuilder();
+
+                using (StreamReader streamReader = new StreamReader(file))
+                {
+                    string currentLine;
+
+                    while (streamReader.Peek() >= 0)
+                    {
+                        currentLine = streamReader.ReadLine();
+
+                        if (currentLine.Contains(prodString) || currentLine.Contains(techString))
+                        {
+                            continue;
+                        }
+
+                        stringBuilder.AppendLine(currentLine);
+                    }
+                }
+
+                using (StreamWriter streamWriter = new StreamWriter(file))
+                {
+                    streamWriter.Write(stringBuilder.ToString());
+                }
+            }
+        }
+    }
+}
